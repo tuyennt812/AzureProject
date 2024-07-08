1415e310a32c0938cb79414331c7c19971b26045
tuyennt812 <114427590+tuyennt812@users.noreply.github.com>
2024-07-08 12:13:58 +0700

    Add files via upload

---
+++ setup/setup.sql
@@ -0,0 +1,25 @@
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ CREATE TABLE [dbo].[merged_data](
+    [customer_id] [int] IDENTITY NOT NULL,
+    [credit_score] [int] NULL,
+    [country] [nvarchar](50) NULL,
+    [gender] [nvarchar](50) NULL,
+    [age] [int] NULL,
+    [tenure] [int] NULL,
+    [balance] [float] NULL,
+    [products_number] [int] NULL,
+    [credit_card] [int] NULL,
+    [active_member] [int] NULL,
+    [estimated_salary] [float] NULL,
+    [churn] [int] NULL)
+
+WITH
+(
+	CLUSTERED COLUMNSTORE INDEX
+);
+GO
+
+
